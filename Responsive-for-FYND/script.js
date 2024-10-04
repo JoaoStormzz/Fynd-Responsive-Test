cd55eca256542328f0dc2a091c5d1380ff3a68f5
João Pedro Seleguim Garcia <136927554+JoaoStormzz@users.noreply.github.com>
2024-10-04 11:41:03 -0300

    Add files via upload

---
+++ Responsive-for-FYND/script.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementById('toggleTheme');
+const body = document.body;
+
+
+toggleButton.addEventListener('click', function () {
+    body.classList.toggle('dark-theme');
+});
